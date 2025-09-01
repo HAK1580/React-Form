a7f268bf95be60f4a8591ca2c2fe9592088c78ca
HAK1580 <aliofficial1580630@gmail.com>
2025-09-01 09:06:31 -0700

    done the change

---
+++ my-app/src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+     <ul>
+        <li> Home</li>
+        <li> About</li>
+        <li> Contacts</li>
+        <li> Download</li>
+     </ul>
+
+    </div>
+  )
+}
+
+export default Navbar
