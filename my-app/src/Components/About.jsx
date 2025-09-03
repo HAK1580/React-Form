bdb3fca5f0c89e9cca26b1abf7c5d63ec60085fe
HAK1580 <aliofficial1580630@gmail.com>
2025-09-03 15:05:46 -0700

    completed now add the responsiveness

--- my-app/src/Components/About.jsx
+++ my-app/src/Components/About.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Links } from 'react-router-dom' 
+import './About.css'
 
 
 const About = () => {
+
   return (
-    <div>
-        <h1>YES I AM About shit here </h1>
+    <div className='about-div'  > 
+        {/* <h1 style={{fontSize:"3.5rem ",marginLeft:"1rem",margin:"4rem auto"}} > ABOUT HEC </h1> */}
+   
+        <a target='_blank' href="https://www.hec.gov.pk/english/aboutus/Pages/aboutus.aspx "> 
+           <img src="about.PNG" alt="" />
+          </a> 
+         <img  src="about2.PNG" alt="" />
+
     </div>
+
   )
 }
 
